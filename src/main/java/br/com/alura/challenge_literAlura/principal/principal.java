add33ccda469a5663fc075f396db138aa3e107e5
suhellynaraujo <github.interface@gmail.com>
2024-07-16 15:22:25 -0300

    iniciando repositorio

---
+++ src/main/java/br/com/alura/challenge_literAlura/principal/principal.java
@@ -0,0 +1,4 @@
+package br.com.alura.challenge_literAlura.principal;
+
+public class principal {
+}
---
+++ src/test/java/br/com/alura/challenge_literAlura/ChallengeLiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge_literAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
