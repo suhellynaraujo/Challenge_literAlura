2a618a5d04b207d3ab32d7f9c7072e4c83ebf9b7
suhellynaraujo <github.interface@gmail.com>
2024-07-16 16:40:59 -0300

    service

--- src/main/java/br/com/alura/challenge_literAlura/ChallengeLiterAluraApplication.java
+++ src/main/java/br/com/alura/challenge_literAlura/ChallengeLiterAluraApplication.java
@@ -1,13 +1,25 @@
 package br.com.alura.challenge_literAlura;
 
+import br.com.alura.challenge_literAlura.principal.Principal;
+import br.com.alura.challenge_literAlura.service.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeLiterAluraApplication {
+public class ChallengeLiterAluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private AutorService service;
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeLiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(service);
+		principal.exibeMenu();
+	}
 }
+
